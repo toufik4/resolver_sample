56673fcf5e454ed48b05dc638a575bcce7aa7696
Toufik ABDELMOUMENE <toufik4@yahoo.fr>
2025-07-24 13:39:05 +0200

    V0

---
+++ README.md
@@ -0,0 +1,17 @@
+# 📦 Résolution dns asynchrone
+
+## 🚀 Description
+Exemple simple de résolution dns asynchrone
+Illustre cas ecole de résolution à travers un boost::asio::io_context
+
+
+## 🛠️ Technologies utilisées
+- C/C++
+- CMake
+- boost::asio
+
+
+## ⚙️ Installation
+git clone ...
+cd ...
+make
---
+++ resolver.cpp
@@ -0,0 +1,65 @@
+// Resolution dns asynchrone
+
+// Puprose:
+// Exemple simple de résolution dns asynchrone
+// Illustre cas ecole de résolution à travers un boost::asio::io_context
+//
+
+// Author : Toufik ABDELMOUMENE
+
+// File: resolver.cpp
+
+
+#include <boost/asio.hpp>
+#include <iostream>
+
+using boost::asio::ip::tcp;
+
+void on_resolve(const boost::system::error_code& ec,
+                tcp::resolver::results_type results)
+{
+    if (ec) {
+        std::cerr << "Erreur de résolution: " << ec.message() << "\n";
+        return;
+    }
+
+    std::cout << "Solve OK:\n";
+    for (const auto& entry : results) {
+        std::cout << entry.endpoint().address().to_string() << "\n";
+    }
+}
+
+int main() {
+
+    try {
+        boost::asio::io_context io_context;
+        
+        tcp::resolver resolver(io_context);
+        std::string host = "www.google.com";
+        std::string port = "80";
+
+        auto lamb = [host](const boost::system::error_code& ec,
+               tcp::resolver::results_type results) {
+            if (ec) {
+                std::cerr << "Erreur pour " << host << ": " << ec.message() << "\n";
+                return;
+            }
+
+            std::cout << "Solving " << host << " OK:\n";
+            for (const auto& entry : results) {
+                std::cout << entry.endpoint().address().to_string() << "\n";
+            }
+        };
+        // Démarre la résolution asynchrone
+        resolver.async_resolve(host, port, lamb);
+        
+
+        // Lancement de la boucle d'événements
+        io_context.run();
+    }
+    catch (const std::exception& e) {
+        std::cerr << "Exception: " << e.what() << "\n";
+    }
+
+    return 0;
+}
